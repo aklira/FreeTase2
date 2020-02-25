73826bf0b98d14cc0790d7137893c388222e624c
aklira <akli.rahmoun@outlook.com>
2020-02-25 11:03:41 +0100

    added custom iso params mms connection creation

--- tase2/client/client.py
+++ tase2/client/client.py
@@ -17,16 +17,18 @@ class IccpConnection:
     def __init__(self, 
                  MmsConnection, 
                  RemoteServer,
+                 RemotePort,
                  enabled = None,
                  error = None):
         self.MmsConnection = MmsConnection
         self.RemoteServer = RemoteServer
+        self.RemotePort = RemotePort
         self.enabled = enabled
         self.error = error
 
     def connect_to_server(self):
-        mmsError = 0
-        result = iec61850.MmsConnection_connect(self.MmsConnection, mmsError, self.RemoteServer, 102)
+        mmsError = iec61850.toMmsErrorP()
+        result = iec61850.MmsConnection_connect(self.MmsConnection, mmsError, self.RemoteServer, self.RemotePort)
         return result
 
     def check_connection(self, id_iccp, loop_error):
@@ -40,7 +42,20 @@ class IccpConf:
 
     def __init__(self,
                  remote_server,
-                 ap_title,
+                 remote_port,
+                 keepalive,
+                 tls,
+                 use_defaults,
+                 remote_ap_title,
+                 remote_ae_qual,
+                 remote_tselector,
+                 remote_sselector,
+                 remote_pselector,
+                 local_ap_title,
+                 local_ae_qual,
+                 local_tselector,
+                 local_sselector,
+                 local_pselector,
                  blt_id,
                  blt_version,
                  tase2_version,
@@ -48,8 +63,24 @@ class IccpConf:
                  assoc_id,
                  ae_title,
                  supported_features):
+        # transport parameters
         self.remote_server = remote_server
-        self.ap_title = ap_title
+        self.remote_port = remote_port
+        self.keepalive = keepalive
+        self.tls = tls
+        # iso parameters
+        self.use_defaults = use_defaults
+        self.remote_ap_title = remote_ap_title
+        self.remote_ae_qual = remote_ae_qual
+        self.remote_tselector = remote_tselector
+        self.remote_sselector = remote_sselector
+        self.remote_pselector = remote_pselector
+        self.local_ap_title = local_ap_title
+        self.local_ae_qual = local_ae_qual
+        self.local_tselector = local_tselector
+        self.local_sselector = local_sselector
+        self.local_pselector = local_pselector
+        # tase2 parameters
         self.blt_id = blt_id
         self.blt_version = blt_version
         self.tase2_version = tase2_version
@@ -67,25 +98,54 @@ class IccpConf:
         with open(conf_file, 'r') as config:
             tase2_conf = yaml.load(config, Loader=yaml.FullLoader)
 
-        remote_server = tase2_conf['remote_server']
-        ap_title = tase2_conf['ap_title']
-        blt_id = tase2_conf['blt_id']
-        blt_version = tase2_conf['blt_version']
-        tase2_version = tase2_conf['tase2_version']
-        ds_name = tase2_conf['ds_name']
-        assoc_id = tase2_conf['assoc_id']
-        ae_title = tase2_conf['ae_title']
-        supported_features = tase2_conf['supported_features']
+        # set transport parameters
+        remote_server = tase2_conf['transport_parameters']['remote_server']
+        remote_port = tase2_conf['transport_parameters']['remote_port']
+        keepalive = tase2_conf['transport_parameters']['keepalive']
+        tls = tase2_conf['transport_parameters']['tls']
+        # set iso parameters
+        use_defaults = tase2_conf['iso_parameters']['use_defaults']
+        remote_ap_title = tase2_conf['iso_parameters']['remote_ap_title']
+        remote_ae_qual = tase2_conf['iso_parameters']['remote_ae_qual']
+        remote_tselector = tase2_conf['iso_parameters']['remote_tselector']
+        remote_sselector = tase2_conf['iso_parameters']['remote_sselector']
+        remote_pselector = tase2_conf['iso_parameters']['remote_pselector']
+        local_ap_title = tase2_conf['iso_parameters']['local_ap_title']
+        local_ae_qual = tase2_conf['iso_parameters']['local_ae_qual']
+        local_tselector = tase2_conf['iso_parameters']['local_tselector']
+        local_sselector = tase2_conf['iso_parameters']['local_sselector']
+        local_pselector = tase2_conf['iso_parameters']['local_pselector']
+        # set tase 2 parameters
+        blt_id = tase2_conf['tase2_parameters']['blt_id']
+        blt_version = tase2_conf['tase2_parameters']['blt_version']
+        tase2_version = tase2_conf['tase2_parameters']['tase2_version']
+        ds_name = tase2_conf['tase2_parameters']['ds_name']
+        assoc_id = tase2_conf['tase2_parameters']['assoc_id']
+        ae_title = tase2_conf['tase2_parameters']['ae_title']
+        supported_features = tase2_conf['tase2_parameters']['supported_features']
 
         return cls(remote_server,
-                   ap_title,
-                   blt_id,
-                   blt_version,
-                   tase2_version,
-                   ds_name,
-                   assoc_id,
-                   ae_title,
-                   supported_features)
+                 remote_port,
+                 keepalive,
+                 tls,
+                 use_defaults,
+                 remote_ap_title,
+                 remote_ae_qual,
+                 remote_tselector,
+                 remote_sselector,
+                 remote_pselector,
+                 local_ap_title,
+                 local_ae_qual,
+                 local_tselector,
+                 local_sselector,
+                 local_pselector,
+                 blt_id,
+                 blt_version,
+                 tase2_version,
+                 ds_name,
+                 assoc_id,
+                 ae_title,
+                 supported_features)
 
 
 conn = None
@@ -117,13 +177,45 @@ def start_iccp(conf_file):
     conf = IccpConf.create_from_file(conf_file)
 
 # create IccpConnection
-    mms_connection = iec61850.MmsConnection_create()
+    if (conf.use_defaults == True):
+        mms_connection = iec61850.MmsConnection_create()
+    else:
+        # connecting with custom ISO parameters: TSelector, SSelector, PSelector, AP-Title, AE-Qualifier
+        local_tselector = iec61850.TSelector()
+        local_tselector.size = len(conf.local_tselector)
+        local_tselector.value = conf.local_tselector 
+        local_sselector = iec61850.SSelector()
+        local_sselector.size = len(conf.local_sselector)
+        local_sselector.value = conf.local_sselector
+        local_pselector = iec61850.PSelector()
+        local_pselector.size = len(conf.local_pselector)
+        local_pselector.value = conf.local_pselector 
+        remote_tselector = iec61850.TSelector()
+        remote_tselector.size = len(conf.remote_tselector)
+        remote_tselector.value = conf.remote_tselector 
+        remote_sselector = iec61850.SSelector()
+        remote_sselector.size = len(conf.remote_sselector)
+        remote_sselector.value = conf.remote_sselector 
+        remote_pselector = iec61850.PSelector()
+        remote_pselector.size = len(conf.remote_pselector)
+        remote_pselector.value = conf.remote_pselector  
+        mms_connection = iec61850.MmsConnection_createWisop(conf.local_ap_title,
+                                                            conf.local_ae_qual,
+                                                            local_tselector,
+                                                            local_sselector,
+                                                            local_pselector,
+                                                            conf.remote_ap_title, 
+                                                            conf.remote_ae_qual,
+                                                            remote_tselector,
+                                                            remote_sselector,
+                                                            remote_pselector)
     remote_server = conf.remote_server
-    conn = IccpConnection(mms_connection, remote_server)
+    remote_port = conf.remote_port
+    conn = IccpConnection(mms_connection, remote_server, remote_port)
 
 # create VCC
  # create bilateral table
-    ap_title = conf.ap_title
+    ap_title = conf.remote_ap_title
     blt_id = conf.blt_id
     blt_version = conf.blt_version
     tase2_version = conf.tase2_version
@@ -148,8 +240,7 @@ def start_iccp(conf_file):
 def connect_iccp():
     success = False
     try:
-        conn.connect_to_server()
-        success = True
+        success = conn.connect_to_server()
     except:
         pass
     return success
