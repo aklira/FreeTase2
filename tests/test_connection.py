d900c556e39b61b084ac80c394c2aef1d65de073
aklira <akli.rahmoun@outlook.com>
2020-02-25 11:50:26 +0100

    keep connection alive

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -7,13 +7,18 @@ import sys
 sys.path.insert(0, "..")
 
 import tase2.client.client as tase2client
+from time import sleep
 
 conf_file = 'test_conn_conf.yml'
 
 def main():
     tase2client.start_iccp(conf_file)
+    # create and start connection
     result = tase2client.connect_iccp()
-    print(result)
+    # keep connection alive until user interrupts
+    while 1:
+       print(result)
+       sleep(10)
 
 if __name__ == '__main__':
     main()
